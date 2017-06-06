56a0325532b20b4acb1318e063095f2756002f24
Adrian Cordoba <amcordobag@gmail.com>
2017-06-05 19:43:50 -0600

    Tomela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListComponentComponent } from './list-component/list-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponentComponent
   ],
   imports: [
     BrowserModule
