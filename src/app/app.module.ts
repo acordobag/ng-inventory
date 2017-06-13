a612eed08c0929f31ca0b58e7e5a79c7e36f23db
Adrian Cordoba <amcordobag@gmail.com>
2017-06-13 01:04:21 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,24 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ListComponentComponent } from './list-component/list-component.component';
 import { PickingComponent } from './picking/picking.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-const routes=[{
-  path: 'products', component: ListComponentComponent},
-{ path: 'in-move', component: PickingComponent}
+const routes = [
+  { path: 'products', component: ListComponentComponent },
+  { path: 'in-move', component: PickingComponent },
+  { path: 'login', component: LoginComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponentComponent,
-    PickingComponent
+    PickingComponent,
+    LoginComponent,
+    DashboardComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
